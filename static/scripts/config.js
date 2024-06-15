2affe476e65fff7b36c233032d203c5cbf911f1a
zFlqw <118718052+zFlqwovo@users.noreply.github.com>
2024-06-15 06:44:09 +0000

    up

---
+++ static/scripts/config.js
@@ -0,0 +1,18 @@
+export default {
+  items: [
+    "meme/?.jpg",
+    "meme/rbq.jpg",
+    "meme/啊？.jpg",
+    "meme/坏蛋.jpg",
+    "meme/大冤种.jpg",
+    "meme/我什么都做不到.jpg",
+    "meme/我喜欢你.jpg",
+    "meme/我就特别色.jpg",
+    "meme/打屁股.jpg",
+    "meme/结婚.jpg",
+    "meme/结婚2.jpg",
+    "meme/群主女装gkd.jpg",
+    "meme/蠢萝莉!.jpg",
+    "meme/🥵.jpg",
+  ]
+}
